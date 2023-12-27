4f9e6037aa264a5509e6351f8dc7e07ad88f10e9
Nitin Singh <nitinkumarsingh2368@gmail.com>
2023-12-28 03:27:39 +0530

    Day 3

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -7,7 +7,11 @@ function App() {
 
  let [counter,setCounter]= useState(7)
  /*useState is a hook , between () we have to give default variable value (here variable is counter and we are setting 7 as its default value)
- useState gives two values in array format [] , 0th index is a value , and 1st index is a function , we can write any name for both 
+  useState gives two values in array format [] , 0th index is a value , and 1st index is a function , we can write any name for both.
+  
+  useState accepts an initial state and returns two values:
+  1.The current state.
+  2.A function that updates the state.
   */
 
   //let counter =7
