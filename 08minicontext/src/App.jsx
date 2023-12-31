dd5b18d9e944717c31bb8917045f4f198a44be38
Nitin Singh <nitinkumarsingh2368@gmail.com>
2023-12-31 06:27:14 +0530

    Day 5

---
+++ 08minicontext/src/App.jsx
@@ -0,0 +1,19 @@
+import UserContextProvider from "./Context/UserContextProvider"
+import Login from "./components/Login"
+import Profile from "./components/Profile"
+
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+      <h1></h1>
+      <Login/>
+      <Profile/>
+
+    </UserContextProvider>
+  )
+}
+
+export default App
